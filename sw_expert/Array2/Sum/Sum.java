ed00b0a60d93e92e793d155bdcd35b46b3522004
yonghyun123 <model9960@naver.com>
2018-04-23 14:17:46 +0900

    sum 문제 잘못 이해했었음

--- sw_expert/Array2/Sum/Sum.java
+++ sw_expert/Array2/Sum/Sum.java
@@ -6,18 +6,26 @@ public class Sum {
 	public static Scanner sc;
 	public static int T = 0;
 	public static int [][]board;
+	public static int leftCross;
+	public static int rightCross;
+	public static int []result;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		sc = new Scanner(System.in);
 		
-		while(T < 10){
+		while(T++ < 10){
 			//input data 
 			//문제 잘못이해함
 			int tmpT = sc.nextInt();
-			board = new int[101][101];
-			for(int i = 0; i < 100; i++){
-				for(int j = 0; j < 100; j++){
+			board = new int[100][100];
+			leftCross = 0;
+			rightCross = 0;
+			int idx = 0;
+			int max = 0;
+			
+			for(int i = 0; i < board.length; i++){
+				for(int j = 0; j < board[i].length; j++){
 					board[i][j] = sc.nextInt();
 				}
 			}
@@ -26,13 +34,29 @@ public class Sum {
 			for(int i = 0; i < 100; i++){
 				int rowSum = 0;
 				int colSum = 0;
-				int crossSum = 0;
 				
 				for(int j = 0; j < 100; j++){
 					rowSum += board[i][j];
 					colSum += board[j][i];
+				}	
+				
+				if(rowSum > colSum ){
+					if(max < rowSum) max = rowSum;
+				} else {
+					if(max < colSum) max = colSum;
 				}
+				leftCross += board[i][i];
+				rightCross += board[i][99-i];
 			}
+			
+			if(leftCross > rightCross){
+				if(max < leftCross) max = leftCross;
+			} else {
+				if(max < rightCross) max = rightCross;
+			}
+			
+			
+			System.out.println("#"+tmpT+" "+max);
 			
 		}
 	}
