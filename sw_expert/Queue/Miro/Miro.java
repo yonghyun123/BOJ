d634bf899cadd4814961160b3fbaec91587e1ab6
yonghyun123 <model9960@naver.com>
2018-06-14 12:43:52 +0900

    miro1 dfs

--- sw_expert/Queue/Miro/Miro.java
+++ sw_expert/Queue/Miro/Miro.java
@@ -11,13 +11,18 @@ public class Miro {
 	//우하좌상
 	public static int[] dRow = {0,1,0,-1};
 	public static int[] dCol = {1,0,-1,0};
+	public static int result = 0;
+	public static int[][] visited;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		sc = new Scanner(System.in);
 		while(T++ < 10){
 			int tmpT = sc.nextInt();
+			result = 0;
 			int[][] board = new int[16][16];
+			visited = new int[16][16];
+			
 			for(int i = 0; i < 16; i++){
 				String str = sc.next();
 				for(int j = 0; j < str.length(); j++){
@@ -26,7 +31,36 @@ public class Miro {
 				
 			}
 			System.out.print("#"+T+" ");
-			System.out.println(findBfs(board,1,1));
+			findDfs(board, 1, 1, visited);
+			System.out.println(result);
+//			System.out.println(findBfs(board,1,1));
+		}
+	}
+	
+	public static void findDfs(int[][] board, int row, int col, int[][] visited){
+		for(int i = 0; i < 16; i++){
+			for(int j = 0; j < 16; j++){
+				System.out.print(visited[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		visited[row][col] = 1;
+		if(board[row][col] == 3){
+			result = 1;
+			return;
+		}
+		
+		for(int i = 0; i < 4; i++){
+			int nextRow = dRow[i] + row;
+			int nextCol = dCol[i] + col;
+			if(nextRow < 0 || nextRow > 15 || nextCol < 0 || nextCol > 15) continue;
+			if(visited[nextRow][nextCol] == 1) continue;
+			if(board[nextRow][nextCol] == 0 || board[nextRow][nextCol] ==3){
+				visited[nextRow][nextCol] = 1;
+				findDfs(board, nextRow, nextCol, visited);
+//				visited[nextRow][nextCol] = 0;
+			}
 		}
 	}
 	
