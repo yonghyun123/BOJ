3816422e917bff729f69effbdfd668b25f7e2702
yonghyun123 <model9960@naver.com>
2018-06-16 18:19:40 +0900

    백준 2606 바이러스 bfs

--- boj_기초/BOJ_2606.java
+++ boj_기초/BOJ_2606.java
@@ -1,5 +1,7 @@
 package boj_기초;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BOJ_2606 {
@@ -27,10 +29,11 @@ public class BOJ_2606 {
 			board[row-1][col-1] = board[col-1][row-1] = 1;
 		}
 		
-		dfs(0, visited);
-		for(int i = 0; i < visited.length; i++){
-			if(visited[i]) result++;
-		}
+//		dfs(0, visited);
+//		for(int i = 0; i < visited.length; i++){
+//			if(visited[i]) result++;
+//		}
+		bfs(0);
 		System.out.println(result - 1);
 	}
 	
@@ -41,6 +44,28 @@ public class BOJ_2606 {
 			if(!visited[i] && board[startNode][i] == 1){
 				dfs(i,visited);
 			}
+		}
+	}
+	
+	public static void bfs(int startNode){
+		Queue<Integer> q = new LinkedList<>();
+		boolean []visited = new boolean[nodeNum];
+		
+		visited[startNode] = true;
+		q.add(startNode);
+		
+		while(!q.isEmpty()){
+			int curNode = q.poll();
+			for(int i = 0 ; i < nodeNum; i++){
+				if(!visited[i] && board[curNode][i] == 1){
+					q.add(i);
+					visited[i] = true;
+				}
+			}
+		}
+		
+		for(int i = 0; i < visited.length; i++){
+			if(visited[i]) result++;
 		}
 	}
 
