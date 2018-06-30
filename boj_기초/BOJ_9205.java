d261edfa6987bc457c47f0cc1190b409dd5d871e
yonghyun123 <model9960@naver.com>
2018-06-30 12:09:09 +0900

    맥주마시면서 걷기 성공

--- boj_기초/BOJ_9205.java
+++ boj_기초/BOJ_9205.java
@@ -8,11 +8,12 @@ import java.util.Scanner;
 public class BOJ_9205 {
 	
 	public static Scanner sc = new Scanner(System.in);
+	public static int[] end;
 
 	public static void main(String[] args) {
 		int T = sc.nextInt();
 
-		while(T-- >0){
+		while(T-- > 0){
 			int numOfCu = sc.nextInt();
 			ArrayList<int[]> position = new ArrayList<>();
 
@@ -23,6 +24,7 @@ public class BOJ_9205 {
 				position.add(pos);
 			}
 			int[] start = position.get(0);
+			end = position.get(position.size()-1);
 			System.out.print(bfs(start, position));
 		}
 
@@ -33,27 +35,26 @@ public class BOJ_9205 {
 		Queue<int[]> q = new LinkedList<>();
 		boolean resultFlag = false;
 		boolean[] visited = new boolean[position.size()];
-		
-		visited[0] = true; 
+		 
 		q.add(start);
 		
 		while(!q.isEmpty()){
 			int curX = q.peek()[0];
 			int curY = q.peek()[1];
 			
-			if(curX == position.get(position.size()-1)[0] && curY == position.get(position.size()-1)[1]){
+			if(curX == end[0] && curY == end[1]){
 				resultFlag = true;
 				break;
 			}
 			
 			q.poll();
-			for(int i = 0; i < position.size(); i++){
+			for(int i = 1; i < position.size(); i++){
 				
-				if(visited[i]) continue;
+				
 				int nextX = position.get(i)[0];
 				int nextY = position.get(i)[1];
 				
-				if( Math.abs((nextX - curX)) + Math.abs((nextY - curY)) <= 1000){
+				if(!visited[i] && (Math.abs((nextX - curX)) + Math.abs((nextY - curY)) <= 1000)){
 					int[] nextPos = {nextX, nextY};
 					q.add(nextPos);
 					visited[i] = true;
