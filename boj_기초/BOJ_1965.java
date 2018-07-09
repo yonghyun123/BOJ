197dc05c81c5fc0b9396e18b67c130acb1cba3d3
yonghyun123 <model9960@naver.com>
2018-07-09 14:49:40 +0900

    백준 1965 상자넣기 lis 적응중

--- boj_기초/BOJ_1965.java
+++ boj_기초/BOJ_1965.java
@@ -3,35 +3,33 @@ package boj_기초;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BOJ_11055 {
+public class BOJ_1965 {
+	
 	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		int N = sc.nextInt();
-		
 		int[] arr = new int[N];
-		int[] dp = new int[N];// 수열의 합 dp table
-		int max = 0;
+		int[] dp = new int[N];
 		
 		for(int i = 0; i < N; i++){
 			arr[i] = sc.nextInt();
 		}
-		dp[0] = arr[0];
+		
+		dp[0] = 1;
 		
 		for(int i = 1; i < N; i++){
-			dp[i] = arr[i];
+			dp[i] = 1;
 			
 			for(int j = 0; j < i; j++){
-		
-				if(arr[i] > arr[j] && dp[j] + arr[i] > dp[i]){
-					dp[i] = dp[j]+arr[i];
-				
+				if(arr[i] > arr[j] && dp[j]+1 > dp[i]){
+					dp[i] = dp[j]+1;
 				}
-			
 			}
 		}
 		Arrays.sort(dp);
 		System.out.println(dp[N-1]);
+
 	}
 
 }
